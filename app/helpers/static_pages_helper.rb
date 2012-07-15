d63a4be899f59b4b5f5182c310b7c0b466f8376e
Eoghan McNulty <eoghan.mcnulty@gmail.com>
2012-07-15 13:33:12 +0100

    full_title def into staticpageshelper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+	# This is probably better place to put the
+	# full_title method
+	def full_title(page_title)
+		base_title = 'RoR Tutorial - Sample App'
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
 end
