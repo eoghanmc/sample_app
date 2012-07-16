821560887a81dba878551106b114f7f8e7c43f02
Eoghan McNulty <eoghan.mcnulty@gmail.com>
2012-07-16 22:46:43 +0100

    Finish the signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
 
+  resources :users
+
   root :to => 'static_pages#home'
 
   match '/signup',  to: 'users#new'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name 			'Eoghan McNulty'
+		email 		'eoghan@example.com'
+		password 	'foobar'
+		password_confirmation 'foobar'
+	end
+end
