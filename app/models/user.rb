413fda7460c65abca45374fb787e1d2b7712f4b0
Eoghan McNulty <eoghan.mcnulty@gmail.com>
2012-07-17 17:15:29 +0100

    Sign in & Sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = user.email.downcase }
+  before_save :create_remember_token
 
   validates :name, 	presence: true, length: { maximum: 50 }
 
@@ -13,5 +14,12 @@ class User < ActiveRecord::Base
 
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+
+  private
+
+  	def create_remember_token
+  		self.remember_token = SecureRandom.urlsafe_base64
+  	end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
 	# and do not refer to any column in the db.
 	it { should respond_to(:password) } 
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 	it { should be_valid }
@@ -98,6 +99,11 @@ describe User do
 			it { should_not ==  user_for_invalid_password }
 			specify { user_for_invalid_password.should be_false }
 		end
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 	end
 
 end
